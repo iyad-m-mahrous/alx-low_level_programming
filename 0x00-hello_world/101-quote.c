af1cab0c974b72eaf5ab56e60cd4823b2f8c117c
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-15 02:15:51 -0700

    Advanced Tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful";
+	char *message = "and that piece of art is useful";
 	*message += "\" - Dora Korpar, 2015-10-19\n";
 
 
