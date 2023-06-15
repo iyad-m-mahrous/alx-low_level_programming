2f2bb8a08fba1cc15933fa3c12baa8399b705edf
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-15 00:27:52 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,24 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry Function
+ *
+ * Description: Task 6
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d\n", sizeof(a));
+	printf("Size of a int: %d\n", sizeof(b));
+	printf("Size of a long int: %d\n", sizeof(c));
+	printf("Size of a long long int: %d\n", sizeof(d));
+	printf("Size of a float: %d\n", sizeof(e));
+
+	return (0);
+}
