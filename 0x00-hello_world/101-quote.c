dbaf48ee156bad9dfc0395e87a39c6165769d956
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-15 01:30:29 -0700

    Advanced Taska

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry Function
+ *
+ * Description: Task 101
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, strlen(message));
+
+
+	return (1);
+}
