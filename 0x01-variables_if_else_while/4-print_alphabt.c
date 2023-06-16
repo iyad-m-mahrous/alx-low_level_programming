9db10006507399a88f7d6b6ced280230d6727120
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-16 06:00:31 -0700

    Tasks 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,23 @@
 /**
  * main - Entry Function
  *
- * Description: Task 6
+ * Description: Task 4
  *
  * Return: 0 (Success)
  */
+
+int main(void)
+{
+	char a = 'a';
+
+	do {
+		if (a != 'q' && a != 'e')
+			putchar(a++);
+		else
+			a++;
+	} while (a <= 'z');
+
+	putchar('\n');
+
+	return (0);
+}
