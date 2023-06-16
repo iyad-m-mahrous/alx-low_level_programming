6b98218f3d42f812db0ebaeeb5754003d110ebbc
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-16 06:41:10 -0700

    Tasks 7

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,20 @@
 /**
  * main - Entry Function
  *
- * Description: Task 6
+ * Description: Task 8
  *
  * Return: 0 (Success)
  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (i = 0; i < 6; i++)
+		putchar(i + 'a');
+	putchar('\n');
+
+	return (0);
+}
