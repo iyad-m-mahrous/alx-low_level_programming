f09ba0a02a63ac1e32ac9328612b18ddcfee3f14
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-16 08:34:20 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,32 @@
 /**
  * main - Entry Function
  *
- * Description: Task 6
+ * Description: Task 8
  *
  * Return: 0 (Success)
  */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (j != 99 || i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
