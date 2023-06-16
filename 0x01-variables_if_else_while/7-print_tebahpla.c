edb025543405dcfc2a7d31ebb95076cfb5018c59
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-16 06:28:20 -0700

    Tasks 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,20 @@
 /**
  * main - Entry Function
  *
- * Description: Task 6
+ * Description: Task 2
  *
  * Return: 0 (Success)
  */
+
+int main(void)
+{
+	char a = 'z';
+
+	do {
+		putchar(a--);
+	} while (a >= 'a');
+
+	putchar('\n');
+
+	return (0);
+}
