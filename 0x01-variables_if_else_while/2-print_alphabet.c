e9ba5e7a6477a7d424288ee52e595f46fbd0384b
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-16 05:36:20 -0700

    Tasks 1-3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,20 @@
 /**
  * main - Entry Function
  *
- * Description: Task 6
+ * Description: Task 2
  *
  * Return: 0 (Success)
  */
+
+int main(void)
+{
+	char a = 'a';
+
+	do {
+		putchar(a++);
+	} while (a <= 'z');
+
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,22 @@
 /**
  * main - Entry Function
  *
- * Description: Task 6
+ * Description: Task 2
  *
  * Return: 0 (Success)
  */
+
+int main(void)
+{
+	char a = 'a';
+
+	do {
+		putchar(a++);
+		if (a == ('z' + 1))
+			a = 'A';
+	} while (a != ('Z' + 1));
+
+	putchar('\n');
+
+	return (0);
+}
