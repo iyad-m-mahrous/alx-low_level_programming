2e244c32461bedad2180eca895e707559aa19a81
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-20 06:43:18 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if ((i * j) / 10 != 0)
+				if ((i * j) / 10 == 0)
 					_putchar(' ');
 				else
 					_putchar((i * j) / 10 + '0');
