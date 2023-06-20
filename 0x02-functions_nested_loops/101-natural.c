52f10e03df9e11ca81594e7d5bf825c998b9cc27
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-20 09:06:54 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - Entry Function
- * @n: the number to be used
  *
  * Description: Task 13
  *
@@ -12,7 +11,7 @@ int main(int n)
 {
 	int i, sum = 0;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
