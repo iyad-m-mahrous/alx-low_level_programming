01ad461f832cb3d436d89369aaaa073fc01624a3
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-22 02:39:32 -0700

    Tasks 0 - 5

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+# include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @c: first integer
+ * @b: second integer
+ *
+ * Return: multiplication result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * followed by a new line.
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, j, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j < 10)
+				n = j * 10;
+			else
+				n = j;
+
+			_putchar(n / 10 + '0');
+
+			if (j >= 10)
+				_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+
+}
