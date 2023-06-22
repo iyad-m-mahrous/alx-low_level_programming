317307f5b3a16fadc7de8a1d8c927560d432290d
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-22 08:47:56 -0700

    Tasks 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#define BASE 1000000000
+
+/**
+ * print_number - prints an integer.
+ * @n: the number 
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i, number, new_base;
+	new_base = BASE;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return ;
+	}
+	
+	while((n / new_base) == 0)
+	{
+		new_base = new_base / 10;
+	}
+
+	while (new_base >= 1)
+	{
+		_putchar((n / new_base) + '0');
+		n = n % new_base;
+		new_base = new_base / 10;
+	}
+
+}
