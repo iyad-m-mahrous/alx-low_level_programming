69b33084494b10e128cf6e1a85604d2d43af32ab
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-22 09:51:39 -0700

    Tasks 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,7 +9,7 @@
 
 void print_number(int n)
 {
-	int i, number = n, new_base = 10;
+	int i, number, new_base = 1;
 
 
 	if (n < 0)
@@ -24,16 +24,14 @@ void print_number(int n)
 		return;
 	}
 
+	number = n;
+
 	for (i = 0; (number / 10) > 0 ; i++)
 	{
 		new_base *= 10;
 		number = number / 10;
 	}
 
-	while ((n / new_base) == 0)
-	{
-		new_base = new_base / 10;
-	}
 
 	while (new_base >= 1)
 	{
