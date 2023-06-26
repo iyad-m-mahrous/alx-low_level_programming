c1e64fe9078a350586a6795d5a8969895452f2d3
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-25 21:59:51 -0700

    All

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,11 +14,16 @@ int _atoi(char *s)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (i > 0)
-				if (s[i - 1] == '-')
-					sign = -1;
 			num = (num * 10) + (s[i] - '0');
 		}
+		else if (s[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		else if (s[i] != ' ' && s[i] != '+')
+		{
+			sign = 1;
+		}	
 		else if (num != 0)
 		{
 			break;
