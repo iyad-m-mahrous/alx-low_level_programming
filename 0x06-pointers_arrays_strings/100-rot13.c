282f177a75788df6e09bb321da747a09bd34d951
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-06-29 03:56:58 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,4 +7,18 @@
 
 char *rot13(char *s)
 {
+	char *main_string = s;
+	char A_or_a;
+
+	while (*s != '\0')
+	{
+		if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
+		{
+			A_or_a = (*s & 32) + 'A';
+			*s = ((*s - A_or_a + 13) % 26) + A_or_a;
+		}
+		s++;
+	}
+
+	return (main_string);
 }
