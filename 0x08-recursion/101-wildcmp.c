0b64cae0758444e6d3a9eddb655e89db89b6fe16
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-05 05:29:45 -0700

    Task 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,9 +6,20 @@
  * @s1: string to compare to
  * @s2: string with wild character
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: returns 1 if the strings are identical, otherwise return 0.
  */
+
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0' && *(s2 + 1) != '*' && *(s2 + 1) != '\0')
+			return (0);
+		return (wildcmp(s1, ++s2) || wildcmp(++s1, s2));
+	}
+	return (0);
 }
