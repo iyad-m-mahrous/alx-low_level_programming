dd81d2b6f83610fab016b61cb37d50607039d86a
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-07 05:57:17 -0700

    Tasks 1 - 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,26 +14,32 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc <= 1)
 	{
 		printf("0\n");
 		return (0);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; i++)
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+			{
+				continue;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]))
-		{
-			sum += atoi(argv[i]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-
+		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
