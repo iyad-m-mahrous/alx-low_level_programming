cc916f21289e7caba22e0ebc9e1d2f29cf96513e
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-07 06:06:46 -0700

    Tasks 1 - 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] >= '0' && argv[i][j] <= '9')
 			{
