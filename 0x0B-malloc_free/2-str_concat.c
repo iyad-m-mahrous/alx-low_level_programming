d03458867c71150d0ed53478dc7a5be5e179f8ca
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-10 01:57:35 -0700

    Task 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,4 +12,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	int size1 = 0, size2 = 0, i;
+	char *new;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		size1++;
+	for (i = 0; s2[i] != '\0'; i++)
+		size2++;
+	new = (char *) malloc(sizeof(char) * (size1 + size2 + 1));
+
+	if (!new)
+		return (0);
+	if (new == 1)
+	{
+		*new = '\0';
+		return (new);
+	}
+
+	for (i = 0; i < size1; i++)
+	{
+		new[i] = s1[i];
+	}
+	for (i = size1; i <= size2; i++)
+	{
+		new[i] = s1[i - size1];
+	}
+	return (new);
 }
