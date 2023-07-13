5b5583035bcd66ee6893ef9b28d9444609390907
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-13 05:11:43 -0700

    All

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked -  allocates memory using malloc.
+ * @b: size
+ *
+ * Return: Pointer to allocated memory or normal process termination
+ * with a status value of 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *out;
+
+	out = malloc(b);
+	if (out)
+		return (out);
+	return (98);
+}
