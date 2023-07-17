17679f898766ec6c488fcf080144706bf6beef35
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-17 01:19:11 -0700

    Task 4

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_HEADER
+#define MIAN_HEADER
+
+#define ABS(X) ((X) < 0 ? -(X) : (X))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_HEADER
+#define MIAN_HEADER
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
