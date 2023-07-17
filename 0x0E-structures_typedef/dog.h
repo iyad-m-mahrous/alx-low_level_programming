f0dce36d88290f88e0c3368f96c3064ee55e6898
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-17 01:42:05 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+/**
+ * struct dog - description for a pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: owner
+ *
+ * Description: Dog Struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
