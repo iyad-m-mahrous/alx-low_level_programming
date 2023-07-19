df3fa81cdc7a63644dbe92ee70417f14c4a8f5b0
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-19 10:02:51 -0700

    Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
 
+typedef __SIZE_TYPE__ size_t;
 int _putchar(int);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
