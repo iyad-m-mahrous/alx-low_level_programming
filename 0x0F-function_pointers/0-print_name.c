58c19f3f46e80455d2c22941542d65f9b43f0303
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-19 09:36:38 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_name - writes the character c to stdout
+ * @name: String
+ * @f: Pointer to function with char parameter and no return
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -2,10 +2,8 @@
 #define MAIN_HEADER
 
 int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
