142e4e1a3d903859f98763b73223db80c224a7a4
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-27 06:32:58 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: Pointer to the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+	const list_t *ptr;
+
+	ptr = h;
+	n = 0;
+	while (ptr != NULL)
+	{
+		printf("[%u] %s\n", ptr->len, ptr->str);
+		n++;
+		ptr = ptr->next;
+	}
+	return (n);
+}
