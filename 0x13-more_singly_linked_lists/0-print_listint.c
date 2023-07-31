01357a8170a82681e1375845803dc09ac4874e29
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-31 00:24:31 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints a singly linked list
+ * @h: pointer to head of singly linked list
+ *
+ * Return: Number of elements in list
+ */
+size_t print_listint(const listint_t *h)
+{
+	if (!h)
+		return (0);
+	printf("%d\n", h->n);
+	h = h->next;
+	return (++ print_listint(h));
+}
