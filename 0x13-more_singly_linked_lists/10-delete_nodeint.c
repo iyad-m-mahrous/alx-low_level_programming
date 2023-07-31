1335a786343903f68699c1080a75d5ac2f06907c
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-31 12:32:02 -0700

    fix Task 9

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,6 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *node = *head, *prev = NULL;
 	unsigned int i = 0;
+
 	if (!(*head))
 		return (-1);
 	for (i = 0; i < index; i++)
