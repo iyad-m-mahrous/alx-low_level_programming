7990703bb5d12dd86067a0f0b0e33aafe6d1b9c7
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-08-01 09:33:10 -0700

    Test

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,39 @@
 #include "lists.h"
 
 /**
+ * find_listint_loop - finds a loop in a linked list
+ *
+ * @head: linked list to search
+ *
+ * Return: address of node where loop starts/returns, NULL if no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *node, *next;
+
+	if (!head)
+		return (NULL);
+
+	node = head;
+	next = head->next;
+
+	while (next)
+	{
+		if (next == next->next)
+			return (next);
+		while (node != next)
+		{
+			if (node == next->next)
+				return (node);
+			node = node->next;
+		}
+		node = head;
+		next = next->next;
+	}
+	return (NULL);
+}
+
+/**
  * print_listint_safe - prints a singly linked list (can print lists with loop)
  * @head: head pointer of singly linked list
  *
@@ -11,14 +44,19 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *temp = head;
 	size_t size = 0;
+	listint_t *looped = find_listint_loop((listint_t *)head);
+	int is_looped = 0;
 
 	if (!head)
 		return (size);
+
 	while (temp)
 	{
+		if (temp == looped)
+			is_looped = 1;
 		printf("[%p] %d\n", (void *)temp, temp->n);
 		size++;
-		if (temp->next >= temp)
+		if (looped == temp->next && is_looped)
 		{
 			temp = temp->next;
 			printf("-> [%p] %d\n", (void *)temp, temp->n);
