d0d47a906103cbda15ebc44df037d48aa8bc7c67
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-08-01 01:12:18 -0700

    Fix Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,14 +13,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev, *new_n;
 	unsigned int i = 0;
 
-	if (idx > 0 && (head) == NULL)
+	if (idx > 0 && (*head) == NULL)
 		return (NULL);
 	new_n = malloc(sizeof(listint_t));
 	if (!new_n)
 		return (NULL);
 	new_n->n = n;
 	new_n->next = NULL;
-	if (idx == 0 && (head) == NULL)
+	if (idx == 0 && (*head) == NULL)
 	{
 		(*head) = new_n;
 		return (new_n);
