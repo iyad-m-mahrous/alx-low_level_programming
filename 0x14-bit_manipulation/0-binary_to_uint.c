f82ce2dc89845d48fd10307db93e71ce5ef150da
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-08-02 23:50:11 -0700

    Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include <main.h>
+/**
+ * binary_to_uint - converts a bit string to unsigned int
+ *
+ * @b: string to convert
+ *
+ * Return: decimal unsigned int value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (!b)
+		return (0);
+
+	while (*b != '\0')
+	{
+		if (*b == '0')
+		{
+			result = result << 1;
+		}
+		else if (*b == '1')
+		{
+			result = result << 1;
+			result++;
+		}
+		else
+		{
+			return (0);
+		}
+		b++;
+	}
+	return (result);
+}
