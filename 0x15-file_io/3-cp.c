df7c80e4f904277c67474a575b103273a94ee3a9
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-08-07 04:47:58 -0700

    Task 2

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - copy one file to another, new file with perms 664
+ * usage - cp file_from file_to
+ *
+ * @argc: number of arg
+ * @argv: list of args
+ *
+ * Return: 97 if incorrect num of args
+ * 98 if file_from does not exist or unreadable
+ * 99 if write fails
+ * 100 if file close fails
+ * 0 otherwise
+ */
+int main(int argc, char *argv[])
+{
+
+}
