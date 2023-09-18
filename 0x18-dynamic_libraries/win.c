1a41d04399e93ba7046942d79a4fc22c49cd35d0
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-09-18 04:26:01 -0700

    Task 3

---
+++ 0x18-dynamic_libraries/win.c
@@ -0,0 +1,21 @@
+#include "win.h"
+#include <stdio.h>
+
+/**
+ * check_result - prints you win
+ * @arg1: dummy
+ * @arg2: dummy
+ * @arg3: dummy
+ * @arg4: dummy
+ *
+ * Return: None
+ */
+void check_result(int64_t arg1, int64_t arg2, int arg3, int arg4)
+{
+	(void) arg1;
+	(void) arg2;
+	(void) arg3;
+	(void) arg4;
+
+	printf("--> Please make me win!\n");
+}
---
+++ 0x18-dynamic_libraries/win.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdint.h>
+
+void check_result(int64_t arg1, int64_t arg2, int arg3, int arg4);
+
+#endif
