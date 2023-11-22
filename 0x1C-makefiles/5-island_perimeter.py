fa1ec4b56668d5c0075ecf7db083c8e237853b4d
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-11-22 04:44:38 -0800

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,12 +13,14 @@ def island_perimeter(grid):
     for i in range(row):
         for j in range(column):
             if grid[i][j] == 1:
-                if grid[i-1] and grid[i-1][j] != 1:
+                if i > 0 and grid[i-1][j] != 1:
                     perimeter += 1
-                if grid[i+1] and grid[i+1][j] != 1:
+                if i < (row-1) and grid[i+1][j] != 1:
                     perimeter += 1
                 if j > 0 and grid[i][j-1] != 1:
                     perimeter += 1
-                if j < (column - 1) and grid[i][j+1] != 1:
+                if j < (column-1) and grid[i][j+1] != 1:
+                    perimeter += 1
+                if i == 0 or i == (row-1) or j == 0 or j == (column-1):
                     perimeter += 1
     return perimeter
