f4dfff310df49d4a1c4f48fe9c6bc30086cee0ae
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-11-22 03:49:20 -0800

    Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""5. Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """
+    input: grid is a list of list of integers
+    return: the perimeter of the island described in grid
+    """
+    perimeter = 0
+    row = len(grid)
+    column = len(grid[0]) if row else 0
+    for i in range(row):
+        for j in range(column):
+            if grid[i][j] == 1:
+                if grid[i-1] and grid[i-1][j] != 1:
+                    perimeter += 1
+                if grid[i+1] and grid[i+1][j] != 1:
+                    perimeter += 1
+                if j > 0 and grid[i][j-1] != 1:
+                    perimeter += 1
+                if j < (column - 1) and grid[i][j+1] != 1:
+                    perimeter += 1
+    return perimeter
